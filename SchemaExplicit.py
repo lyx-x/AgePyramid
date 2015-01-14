c7ede075fef687934644246749d39c9de5402606
lyx-x <li.yuxiang.nj@gmail.com>
2015-01-14 03:48:58 +0100

    Correction schema

--- SchemaExplicit.py
+++ SchemaExplicit.py
@@ -1,42 +1,62 @@
-from numpy import arange, zeros, ones;
-from scipy.sparse import spdiags, bmat;
-from matplotlib.pyplot import figure, plot, show;
+import numpy as np
+import scipy.sparse as sp
+import pylab as pl
 
 #Initialisation
 
-a = 1;  #delta_a
-t = 1;  #delta_t
-T = 100;  #Temps
-A = 50;  
-N = (int)(A / a);  #discretisation
-rho = arange(N + 1);
-mu = lambda a, P : 0.0001 * a * P;
+#Série 1, delta_a plus grand que 1/mu, instable
+#a = 5;
+#t = 5;
+
+#Série 2, delta_t plus grand delta_a, instable
+#a = 1;
+#t = 1.5;
+
+#Série 3, stable
+#a = 1;  #delta_a
+#t = 1;  #delta_t
+
+#Série 4, stable
+delta_a = 1;  #delta_a
+delta_t = 0.1;  #delta_t
+Temps = 10000;  #Temps
+T = (int)(Temps / delta_t);  #Cycle
+A = 100;  
+N = (int)(A / delta_a);  #discretisation
+
+mu = lambda a, P : 0.001 * a * P;
+#mu = lambda a, P : P / (A - a);
 betaFunction = lambda a : (a <= 40) * (a >= 20);
-initFunction = lambda a : (a <= 5) * (a >= 0);
-Age = arange(N + 1) * a;
-beta = betaFunction(Age);  #Taux de naissance
+initFunction = lambda a : (a <= 5) * (a >= 0);  #Population initiale
+
+Age = np.arange(N + 1) * delta_a;
+rho = np.arange(N + 1) * delta_a;
 rho = initFunction(rho) * 1;
-population = sum(rho);
+population = sum(rho) * delta_a;
+
 
 #Matrice
 
-diagonal = ones(N + 1) * (1 - t / a);
-subDiag = ones(N + 1) * (t / a - mu(a * Age, population));
-trans = bmat([[beta],[spdiags([subDiag, diagonal], [0, 1], N, N + 1)]]);
+beta = betaFunction(Age) * delta_a;  #Taux de naissance, première ligne
+diagonal = np.ones(N + 1) * (1 - delta_t / delta_a);
+subDiag = np.ones(N + 1) * (delta_t / delta_a - mu(delta_a * Age - delta_a, population) * delta_t);
+trans = sp.bmat([[beta],[sp.spdiags([subDiag, diagonal], [0, 1], N, N + 1)]]);
 
-#Transition
+#Evolution
 
-figure('Evolution');
+pl.ion()
+line, = pl.plot(Age, rho) 
+
 for n in range(T):
     if (n % 1 == 0):
-        plot(Age, rho);
+        line.set_ydata(rho)
+        pl.draw() 
     rho = trans.dot(rho);
-    population = sum(rho);
-    newSubDiag = ones(N + 1) * (t / a - mu(a * Age, population));
-    trans = trans + bmat([[zeros(N + 1)],[spdiags([newSubDiag - subDiag], [0], N, N + 1)]]);
+    population = sum(rho) * delta_a;
+    newSubDiag = np.ones(N + 1) * (delta_t / delta_a - mu(delta_a * Age - delta_a, population) * delta_t);
+    trans = trans + sp.bmat([[np.zeros(N + 1)],[sp.spdiags([newSubDiag - subDiag], [0], N, N + 1)]]);  #Nouvelle matrice
     subDiag = newSubDiag;
-show();
 
-
-
+pl.ioff()
+pl.show()
 
